16ce764a2d1415e158f3c558b0918ba5c1a7af32
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2013-11-28 01:04:12 -0500

    Added some testing code under test/ folder.

---
+++ test/itensor_test.C
@@ -0,0 +1,35 @@
+#include "itensor/itensor.h"
+
+using boost::format;
+using namespace std;
+
+int 
+main(int argc, char* argv[])
+    {
+
+    Index i1("i1",2,Site),
+          l2("l2",10);
+
+    Print(i1);
+    Print(l2);
+
+    ITensor T(i1,l2);
+    T.randomize();
+
+    ITensor T1(l2);
+    T1.randomize();
+
+    PrintDat(T1);
+
+    PrintDat(T);
+
+    Print(T(i1(1),l2(1)));
+    Print(T(i1(1),l2(2)));
+    Print(T(i1(1),l2(3)));
+    Print(T(i1(1),l2(4)));
+
+    Print(T(i1(2),l2(5)));
+    Print(T(l2(5),i1(2)));
+
+    return 0;
+    }

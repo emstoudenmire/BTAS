44628d8dc252704da70a9f8da84dc390ff8bcc83
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-01-03 16:35:41 -0500

    Made error.h header-only.

--- itensor/error.h
+++ itensor/error.h
@@ -7,10 +7,7 @@
 
 #include <stdexcept>
 #include <iostream>
-
-void error(const std::string& s);
-void error(const std::string& s, int line,const char* file);
-#define Error(exp)  error(exp, __LINE__, __FILE__)
+#include <cstdlib>
 
 /*
 class ITError : public std::runtime_error
@@ -29,6 +26,32 @@ class ITError : public std::runtime_error
     */
 
 namespace itensor {
+
+void inline
+error(const std::string& s)
+    {
+    std::cerr << std::endl << s << std::endl;
+    std::cout << std::endl << s << std::endl;
+    std::cout.flush();
+    abort();
+    }
+
+void inline
+error(const std::string& s, int line,const char* file = 0)
+    {
+    std::cerr << "From line " << line;
+    if(file != 0) std::cerr << ", file " << file;
+    std::cerr << std::endl;
+
+    std::cerr << std::endl << s << std::endl;
+    std::cout << std::endl << s << std::endl;
+    std::cout.flush();
+    std::cerr.flush();
+    abort();
+    }
+
+#define Error(exp)  error(exp, __LINE__, __FILE__)
+
 
 class ITError
     {
